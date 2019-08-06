546d873e78bde6a887ca5bc164348c28276e50dd
Madhu Gumballi <madhu.gumballi@gmail.com>
2019-08-06 21:33:54 +0530

    Refactoring
    Refactored code

--- extensio_mno_clusters.py
+++ extensio_mno_clusters.py
@@ -5,6 +5,24 @@ Created on Sun Jul 14 07:31:55 2019
 @author: madhugumballi
 """
 
+def plotCountry(country_code, data, plt) :
+    data = data.loc[data['mcc'] == country_code]
+
+    latitude_list = data['lat']  
+    longitude_list = data['lon']
+    
+    coordinates = np.column_stack((longitude_list, latitude_list))
+    
+    x, y = kmeans2(whiten(coordinates), 
+                   12,
+                   minit="points")
+    plt.figure(figsize=(10.5, 8))
+    plt.scatter(coordinates[:,0], coordinates[:,1], 
+                c=y, 
+                marker="o",
+                s=1,
+                alpha=0.1);    
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,22 +31,8 @@ from scipy.cluster.vq import kmeans2, whiten
 data = pd.read_csv("mexico.csv")
 pd.set_option('display.max_columns', None)
 
-#334 is Mexico code
-data1 = data.loc[data['mcc'] == 334]
-#data1.to_csv(r'mexico_filtered.csv')
+#334 is Mexico country code
+country_code = 334
+plotCountry(country_code, data, plt)
 
-latitude_list = data1['lat']  
-longitude_list = data1['lon']
-
-coordinates = np.column_stack((longitude_list, latitude_list))
-
-x, y = kmeans2(whiten(coordinates), 
-               12,
-               minit="points")
-plt.figure(figsize=(10.5, 8))
-plt.scatter(coordinates[:,0], coordinates[:,1], 
-            c=y, 
-            marker="o",
-            s=1,
-            alpha=0.1);
 plt.show()
--- mexico_mno_generations.py
+++ mexico_mno_generations.py
@@ -6,6 +6,23 @@ Created on Sun Jul 28 21:44:31 2019
 @author: madhugumballi
 """
 
+def plotGeneration(generation, country_code, data, plt) :
+    data1 = data.loc[data['mcc'] == country_code]
+    result = data1.loc[data1['radio'] == generation]
+    
+    latitude_list = result['lat']  
+    longitude_list = result['lon']
+    
+    coordinates = np.column_stack((longitude_list, latitude_list))
+    
+    plt.figure(figsize=(10.5, 8))
+    plt.scatter(coordinates[:,0], 
+                coordinates[:,1], 
+                marker="o",
+                c="b",
+                s=1,
+                alpha=0.1);
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -15,52 +32,8 @@ data = pd.read_csv("mexico.csv")
 pd.set_option('display.max_columns', None)
 
 #334 is Mexico code
-data1 = data.loc[data['mcc'] == 334]
-#data1.to_csv(r'mexico_filtered.csv')
-
-gsm = data1.loc[data1['radio'] == 'GSM']
-
-latitude_list = gsm['lat']  
-longitude_list = gsm['lon']
-
-coordinates = np.column_stack((longitude_list, latitude_list))
-
-plt.figure(figsize=(10.5, 8))
-plt.scatter(coordinates[:,0], 
-            coordinates[:,1], 
-            marker="o",
-            c="b",
-            s=1,
-            alpha=0.1);
-            
-umts = data1.loc[data1['radio'] == 'UMTS']
-
-latitude_list = umts['lat']  
-longitude_list = umts['lon']
-
-coordinates = np.column_stack((longitude_list, latitude_list))
-
-plt.figure(figsize=(10.5, 8))
-plt.scatter(coordinates[:,0], 
-            coordinates[:,1], 
-            marker="x",
-            c="g",
-            s=1,
-            alpha=0.1);
-            
-lte = data1.loc[data1['radio'] == 'LTE']
-
-latitude_list = lte['lat']  
-longitude_list = lte['lon']
-
-coordinates = np.column_stack((longitude_list, latitude_list))
-
-plt.figure(figsize=(10.5, 8))
-plt.scatter(coordinates[:,0], 
-            coordinates[:,1], 
-            marker="*",
-            c="r",
-            s=1,
-            alpha=0.1);
+plotGeneration('GSM', 334, data, plt)
+plotGeneration('UMTS', 334, data, plt)
+plotGeneration('LTE', 334, data, plt)
             
 plt.show()
